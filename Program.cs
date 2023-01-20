695ff0caa4db223230105bb32233a42326f31877
Praful Gabhane <45679855+prafulgabhane@users.noreply.github.com>
2023-01-20 14:55:33 +0530

    Adding files from local to remote

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿//UC-1-Given a Message, ability to analyse and respond Happy or Sad Mood
+namespace MoodAnalyzer
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string message;
+            Random random = new Random();
+            int mood = random.Next(2);
+
+            if (mood == 1)
+            {
+                message = "Happy";
+            }
+            else
+            {
+                message = "Sad";
+
+            }
+            Console.WriteLine(MoodAnalyzer.analyzeMood(message));
+            Console.ReadLine();
+        }
+    }
+}
