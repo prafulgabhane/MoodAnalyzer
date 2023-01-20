79281b8a244e09bb169664e5ea50c9e0f17a4837
Praful Gabhane <45679855+prafulgabhane@users.noreply.github.com>
2023-01-20 14:54:42 +0530

    Adding files from local to remote

---
+++ MoodAnalyzer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.SymbolStore;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyzer
+{
+    public class MoodAnalyzer
+    {
+        public static string analyzeMood(string message)
+        {
+            return message;
+        }
+    }
+}
